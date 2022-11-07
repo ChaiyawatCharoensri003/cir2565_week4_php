39d92d7e683100bedb0ee43ab47ba2a886645304
ChaiyawatCharoensri003 <airsupansa23@gmail.com>
2022-11-07 11:55:55 +0700

    first commit

---
+++ app402.php
@@ -0,0 +1,11 @@
+<?php
+//1.Arithemetic Operator
+$x = 10;
+
+echo "1.Addition     and Assign Value $x + $y =".($x + $y=20)."<by>"; //30
+echo "2.Subtraction  and Assign Value $x - $y =".($x - $y=20)."<by>"; //10
+echo "3.Multiply     and Assign Value $x * $y =".($x * $y=20)."<by>"; //200
+echo "4.Division     and Assign Value $x / $y =".($x / $y=20)."<by>"; //10
+echo "5.Modulus      and Assign Value $x % $y =".($x % $y=20)."<by>"; //10
+echo "6.Exponential  and Assign Value $x * * $y =".($x ** $y=20)."<by>"; //
+?>
---
+++ app404.php
@@ -0,0 +1,18 @@
+<?php
+//4.Increment/Decreatment
+$x = 10;
+$y = 10;
+
+// echo "1.Postfig ++x:"++$x."<br>";//Postfig x; $x = $y + 1;//$x = 10 + 1 //11
+// echo "2.Postfig ++y:"++$y."<br>";//Postfig y; $x = $y + 1;//$x = 10 + 1 //11
+
+// echo "3.Prefig x++:"$x++."<br>";//Prefig x; $x = $x;//$x = 11
+// echo "4.Prefig x++:"$x++."<br>";//Prefig x; $x = $x + 1 ;//$x = 12
+
+// echo "5.Prefig y++:"$y++."<br>";//Prefig y; $x = $x;//$x = 11
+// echo "6.Prefig y++:"$y++."<br>";//Prefig y; $x = $x + 1 ;//$x = 12
+
+echo "7.Postfig x--:".$x-- ."<br>";//Postfig x; $x = $x - 1;//$x = 10 - 1 //10
+
+echo "8.Postfig --y:".--$y ."<br>";//Postfig y; $x = $y - 1;//$x = 10 - 1 //9
+?>
